7040fc2a13cea378253a4452c38370e4b46c8108
hadomanh <hadomanh99@gmail.com>
2021-09-24 17:07:06 +0700

    Update about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -10,28 +10,10 @@
             <div class="col-lg-8">
                 <div class="about">
                     <p><img src="{{ asset('img/about-img.png') }}" alt=""></p>
-                    <p>FTX Fund (FTXF) is a token issued by FTX Finance Ltd based in the UK , the company is backed
-                        by CMC Holding based in the US. </p>
-                    <p>The goal of the FTX Fund project is to serve symbiotic ecosystems, use the FTXF token as the
-                        backbone to convert traditional transactions into digital transactions on the Blockchain
-                        platform and the utility of cryptocurrencies.</p>
-                    <p>The smart contract platform of the FTXF token is programmed and based on the next generation
-                        Binance Smart Chain (BSC) platform by an experienced Blockchain team. FTX Finance issues
-                        FTXF tokens towards multi - purpose, focusing on activities to support related financial
-                        services such as e-commerce, investment, cross-border payments or other services related to
-                        Fintech, including global payment gateways.</p>
-                    <p>CMC Chain is a district Blockchain network giving security token custody to digital
-                        asset managers, helping businesses and investors tokenize their corporate assets,
-                        securities, real estate easily approach investors around the world, and easily raise funds
-                        quickly and conveniently. This also reduces many costs for intermediaries, managers and
-                        complicated legal procedures and ensures transparency in its asset portfolio management,
-                        increases liquidity in the market, makes fast and convenient transactions with low
-                        transaction fees.</p>
-                    <p>CMC Holding will back its member companies, in which FTX Finance is a typical example for the
-                        digital assets and financial technology market with an experienced team that will be a solid
-                        foundation, bringing about bold breakthroughs. With the most practical ecosystem, the scale,
-                        the most methodical investment, FTX Fund is expected to be a pioneer project creating strong
-                        leverage for the next products of FTX Finance in particular and CMC Holdings in general.</p>
+                    <p>FTX Fund (FTXF) is a token issued by FTX Finance Ltd registered in the UK and backed by <a href="https://cmcholdings.com/" target="_blank">CMC Holdings Inc</a> in the USA.</p>
+                    <p>The FTX Fund project was conceived to serve as bridge between symbiotic ecosystems. The FTXF token is the backbone of this environment that transfers traditional into digital transactions on the blockchain.</p>
+                    <p>The smart contract platform of the FTXF token was created by our experienced blockchain team and is based on the next generation <a href="https://www.binance.org/en/smartChain" target="_blank">Binance Smart Chain (BSC)</a>. All FTXF tokens are issued with multi- purpose utility in mind, focusing on activities in support of related financial services in fields such as e-commerce, investments and cross-border payments including via global payment gateways.</p>
+                    
                 </div>
             </div>
         </div>
